fecec6c68656842df08d937cb7081af60bd472c3
Daniel Segura <danielseguraf@gmail.com>
2021-10-20 23:10:35 -0500

    update app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
 
-function App() {
+const App = () => {
   return (
     <>
       <Header />
