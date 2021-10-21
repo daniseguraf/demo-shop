e0af7f78ca8039ab59376c69e03c4eda34ea0cfc
Daniel Segura <danielseguraf@gmail.com>
2021-10-20 22:58:41 -0500

    home screen product listing

---
+++ frontend/src/components/Product.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Card } from 'react-bootstrap';
+
+const Product = ({ _id, image, name, rating, numReviews, price }) => {
+  return (
+    <Card className="my-3 p-3 rounded">
+      <a href={`/product/${_id}`}>
+        <Card.Img src={image} />
+      </a>
+      <Card.Body>
+        <a href={`/product/${_id}`}>
+          <Card.Title as="div">
+            <strong>{name}</strong>
+          </Card.Title>
+        </a>
+
+        <Card.Text as="div">
+          <div className="my-3">
+            {rating} from {numReviews}
+          </div>
+        </Card.Text>
+
+        <Card.Text as="h3">${price}</Card.Text>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default Product;
