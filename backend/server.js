3385a9474e8fb8792dbfeaf9d914efa4881274ab
Daniel Segura <danielseguraf@gmail.com>
2021-10-21 19:16:12 -0500

    serving products - backend routes

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const products =require('./data/products')
+
+const app = express();
+
+app.get('/', (req, res) => res.send('API running...'))
+
+app.get('/api/products', (req, res) => res.json(products))
+
+app.get('/api/products/:id', (req, res) => {
+  const product = products.find(el => el._id === req.params.id)
+  res.json(product)
+})
+
+
+app.listen(5000, console.log('Server running on port 5000 '))
