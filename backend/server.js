9dfa9745d31e0263ee6eaac23e075ea3b14294c2
Daniel Segura <danielseguraf@gmail.com>
2021-10-22 00:33:40 -0500

    connecting to the database

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,18 @@
 import express from  'express'
 import dotenv from  'dotenv'
+
+import connectDB from './config/db.js'
 import products from './data/products.js'
 
 dotenv.config()
+
+connectDB()
+
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 const ENVIROMENT = process.env.NODE_ENV;
+
 
 app.get('/', (req, res) => res.send(`API running on ${PORT}`))
 
