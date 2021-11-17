4bada1e81eb2386581f20ad8ffebba050b7d8784
Daniel Segura <danielseguraf@gmail.com>
2021-11-16 23:02:18 -0500

    fetching products

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,15 +1,14 @@
-import mongoose from 'mongoose'
-
+import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {})
+    const conn = await mongoose.connect(process.env.MONGO_URI, {});
 
     console.log(`Mongo connected: ${conn.connection.host}`.cyan.underline);
   } catch (error) {
     console.error(`Error: ${error.message}`.red.underline.bold);
-    process.exit(1)
+    process.exit(1);
   }
-}
+};
 
-export default connectDB
+export default connectDB;
--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,23 @@
-import express from  'express'
-import dotenv from 'dotenv'
-import colors from 'colors'
-import connectDB from './config/db.js'
-import products from './data/products.js'
+import express from 'express';
+import dotenv from 'dotenv';
+import colors from 'colors';
+import connectDB from './config/db.js';
 
-dotenv.config()
+import productRoutes from './routes/productRoutes.js';
 
-connectDB()
+dotenv.config();
+connectDB();
 
 const app = express();
+
+app.use('/api/products', productRoutes);
 
 const PORT = process.env.PORT || 5000;
 const ENVIROMENT = process.env.NODE_ENV;
 
+app.get('/', (req, res) => res.send(`API running on ${PORT}`));
 
-app.get('/', (req, res) => res.send(`API running on ${PORT}`))
-
-app.get('/api/products', (req, res) => res.json(products))
-
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find(el => el._id === req.params.id)
-  res.json(product)
-})
-
-
-app.listen(PORT, console.log(`Server running in ${ENVIROMENT} mode on ${PORT}`.magenta.bold))
+app.listen(
+  PORT,
+  console.log(`Server running in ${ENVIROMENT} mode on ${PORT}`.magenta.bold)
+);
