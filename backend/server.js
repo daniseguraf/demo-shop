865c2eb143f33efa4795e9f8ce2c50805c0f0eb3
Daniel Segura <danielseguraf@gmail.com>
2021-11-17 22:43:58 -0500

    create a redux store

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import colors from 'colors';
 import connectDB from './config/db.js';
 
 import productRoutes from './routes/productRoutes.js';
+import { notFound, errorHandler } from './/middleware/errorMiddleware.js';
 
 dotenv.config();
 connectDB();
@@ -11,6 +12,10 @@ connectDB();
 const app = express();
 
 app.use('/api/products', productRoutes);
+
+app.use(notFound);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 const ENVIROMENT = process.env.NODE_ENV;
