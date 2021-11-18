d4d9235401ee312c345a74d543835349109a4a41
Daniel Segura <danielseguraf@gmail.com>
2021-11-18 00:50:46 -0500

    bringing state to HomeScreen

---
+++ frontend/src/reducers/productReducers.js
@@ -0,0 +1,21 @@
+import {
+  PRODUCT_LIST_REQUEST,
+  PRODUCT_LIST_SUCCESS,
+  PRODUCT_LIST_FAIL,
+} from '../constants/productConstants';
+
+const productListReducer = (state = { products: [] }, action) => {
+  if (action.type === PRODUCT_LIST_REQUEST) {
+    return { loading: true, products: [] };
+  }
+  if (action.type === PRODUCT_LIST_SUCCESS) {
+    return { loading: false, products: action.payload };
+  }
+  if (action.type === PRODUCT_LIST_FAIL) {
+    return { loading: false, error: action.payload };
+  }
+
+  return state;
+};
+
+export { productListReducer };
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,26 +1,35 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { productActions } from '../actions/productActions';
 
 import { Row, Col } from 'react-bootstrap';
 import Product from './../components/Product';
 
 const HomeScreen = () => {
-  const [products, setProducts] = useState([]);
+  const dispatch = useDispatch();
+  const productList = useSelector((state) => state.productList);
+  const { loading, error, products } = productList;
 
   useEffect(() => {
-    axios.get('/api/products').then((res) => setProducts(res.data));
-  }, []);
+    dispatch(productActions());
+  }, [dispatch]);
 
   return (
     <>
       <h1>Latest Products</h1>
-      <Row>
-        {products.map((el) => (
-          <Col key={el._id} sm={12} md={6} lg={4}>
-            <Product {...el} />
-          </Col>
-        ))}
-      </Row>
+      {loading ? (
+        <h2>Loading...</h2>
+      ) : error ? (
+        <h3>{error}</h3>
+      ) : (
+        <Row>
+          {products.map((el) => (
+            <Col key={el._id} sm={12} md={6} lg={4}>
+              <Product {...el} />
+            </Col>
+          ))}
+        </Row>
+      )}
     </>
   );
 };
