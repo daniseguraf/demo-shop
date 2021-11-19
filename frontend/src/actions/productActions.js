9c52f4223196d7101ce180a1f0ddf6dd7ebd0e29
Daniel Segura <danielseguraf@gmail.com>
2021-11-18 22:50:41 -0500

    product details, reducer and action

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -3,6 +3,9 @@ import {
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
   PRODUCT_LIST_FAIL,
+  PRODUCT_DETAIL_REQUEST,
+  PRODUCT_DETAIL_SUCCESS,
+  PRODUCT_DETAIL_FAIL,
 } from '../constants/productConstants';
 
 const productActions = () => {
@@ -25,4 +28,24 @@ const productActions = () => {
   };
 };
 
-export { productActions };
+const productDetailActions = (id) => {
+  return async (dispatch) => {
+    try {
+      dispatch({ type: PRODUCT_DETAIL_REQUEST });
+
+      const { data } = await axios.get(`/api/products/${id}`);
+
+      dispatch({ type: PRODUCT_DETAIL_SUCCESS, payload: data });
+    } catch (error) {
+      dispatch({
+        type: PRODUCT_DETAIL_FAIL,
+        payload:
+          error.response && error.response.data.message
+            ? error.response.data.message
+            : error.message,
+      });
+    }
+  };
+};
+
+export { productActions, productDetailActions };
--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -2,11 +2,14 @@ import {
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
   PRODUCT_LIST_FAIL,
+  PRODUCT_DETAIL_REQUEST,
+  PRODUCT_DETAIL_SUCCESS,
+  PRODUCT_DETAIL_FAIL,
 } from '../constants/productConstants';
 
-const productListReducer = (state = { products: [] }, action) => {
+const productListReducer = (slice = { products: [] }, action) => {
   if (action.type === PRODUCT_LIST_REQUEST) {
-    return { loading: true, products: [] };
+    return { loading: true, ...slice };
   }
   if (action.type === PRODUCT_LIST_SUCCESS) {
     return { loading: false, products: action.payload };
@@ -15,7 +18,21 @@ const productListReducer = (state = { products: [] }, action) => {
     return { loading: false, error: action.payload };
   }
 
-  return state;
+  return slice;
 };
 
-export { productListReducer };
+const productDetailReducer = (slice = { product: { reviews: [] } }, action) => {
+  if (action.type === PRODUCT_DETAIL_REQUEST) {
+    return { loading: true, ...slice };
+  }
+  if (action.type === PRODUCT_DETAIL_SUCCESS) {
+    return { loading: false, product: action.payload };
+  }
+  if (action.type === PRODUCT_DETAIL_FAIL) {
+    return { loading: false, error: action.payload };
+  }
+
+  return slice;
+};
+
+export { productListReducer, productDetailReducer };
