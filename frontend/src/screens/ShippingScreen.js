03c9d8f5947fec8076a69ba052a83dbab2bff942
Daniel Segura <danielseguraf@gmail.com>
2021-12-09 23:57:38 -0500

    checkout steps component

--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
-import { Form, Button, Row, Col } from 'react-bootstrap';
+import { Form, Button } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
-import Message from '../components/Message';
 import FormContainer from '../components/FormContainer';
+import CheckoutSteps from '../components/CheckoutSteps';
 
 import { saveShippingAddress } from '../actions/cartActions';
 
@@ -24,6 +24,7 @@ const ShippingScreen = ({ history }) => {
   return (
     <FormContainer>
       <h1>Shipping</h1>
+      <CheckoutSteps step1 step2 />
 
       <Form onSubmit={submitHandler}>
         <Form.Group controlId="address">
