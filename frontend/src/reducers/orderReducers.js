24e7e8a5318414fc093296dcfb1a55ee25082ea0
Daniel Segura <danielseguraf@gmail.com>
2021-12-12 01:52:05 -0500

    order details reducer and action

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -2,6 +2,9 @@ import {
   ORDER_CREATE_REQUEST,
   ORDER_CREATE_SUCCESS,
   ORDER_CREATE_FAIL,
+  ORDER_DETAILS_REQUEST,
+  ORDER_DETAILS_SUCCESS,
+  ORDER_DETAILS_FAIL,
 } from '../constants/orderConstants';
 
 export const orderCreateReducer = (state = {}, action) => {
@@ -12,6 +15,23 @@ export const orderCreateReducer = (state = {}, action) => {
     return { loading: false, order: action.payload, success: true };
   }
   if (action.type === ORDER_CREATE_FAIL) {
+    return { loading: false, error: action.payload };
+  }
+
+  return state;
+};
+
+export const orderDetailsReducer = (
+  state = { orderItems: [], shippingAddress: {} },
+  action
+) => {
+  if (action.type === ORDER_DETAILS_REQUEST) {
+    return { ...state, loading: true };
+  }
+  if (action.type === ORDER_DETAILS_SUCCESS) {
+    return { loading: false, order: action.payload };
+  }
+  if (action.type === ORDER_DETAILS_FAIL) {
     return { loading: false, error: action.payload };
   }
 
