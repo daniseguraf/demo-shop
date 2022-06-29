f296960e7b0a762a756c11973ef8ced641333465
Daniel Segura <danielseguraf@gmail.com>
2022-06-29 02:23:56 -0500

    added header

--- frontend/src/app/App.js
+++ frontend/src/app/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 
-// import Header from '../components/Header';
+import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 import HomeScreen from '../screens/HomeScreen';
@@ -19,7 +19,7 @@ import CartScreen from '../screens/CartScreen';
 const App = () => {
   return (
     <BrowserRouter>
-      {/* <Header /> */}
+      <Header />
       <main className="py-3">
         <Container>
           <Routes>
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,48 +1,46 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { LinkContainer } from 'react-router-bootstrap';
+import { Link } from 'react-router-dom';
 import { Container, Navbar, Nav, NavDropdown } from 'react-bootstrap';
-import { userLogout } from '../actions/userActions';
+// import { userLogout } from '../actions/userActions';
 
 const Header = () => {
   const dispatch = useDispatch();
-  const { userInfo } = useSelector((state) => state.userLogin);
+  // const { userInfo } = useSelector((state) => state.userLogin);
 
   const handleLogout = () => {
-    dispatch(userLogout());
+    // dispatch(userLogout());
   };
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
-          <LinkContainer to="/">
+          <Link to="/">
             <Navbar.Brand>DemoShop</Navbar.Brand>
-          </LinkContainer>
+          </Link>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto">
-              <LinkContainer to="/cart">
-                <Nav.Link>
-                  <i className="fas fa-shopping-cart"></i> Cart
-                </Nav.Link>
-              </LinkContainer>
+              <Link to="/cart">
+                <i className="fas fa-shopping-cart"></i> Cart
+              </Link>
 
-              {userInfo ? (
+              {/* {userInfo ? (
                 <NavDropdown title={userInfo.name} id="username">
-                  <LinkContainer to="/profile">
+                  <Link to="/profile">
                     <NavDropdown.Item>Profile</NavDropdown.Item>
-                  </LinkContainer>
+                  </Link>
                   <NavDropdown.Item onClick={handleLogout}>
                     Logout
                   </NavDropdown.Item>
                 </NavDropdown>
               ) : (
-                <LinkContainer to="/login">
+                <Link to="/login">
                   <Nav.Link>
                     <i className="fas fa-user"></i> Sign In
                   </Nav.Link>
-                </LinkContainer>
-              )}
+                </Link>
+              )} */}
             </Nav>
           </Navbar.Collapse>
         </Container>
