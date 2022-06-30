77261af5f9c7b9b221397be793ecfb18ab8f3da2
Daniel Segura <danielseguraf@gmail.com>
2022-06-30 00:17:29 -0500

    refactored user controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,7 +5,7 @@ import User from '../models/userModel.js';
 // @desc Auth user & get token
 // @route POST /api/users/login
 // @access Public
-const authUser = asyncHandler(async (req, res) => {
+export const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
 
   const user = await User.findOne({ email });
@@ -27,7 +27,7 @@ const authUser = asyncHandler(async (req, res) => {
 // @desc Register a new user
 // @route POST /api/users
 // @access Public
-const registerUser = asyncHandler(async (req, res) => {
+export const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
 
   const userExists = await User.findOne({ email });
@@ -56,7 +56,7 @@ const registerUser = asyncHandler(async (req, res) => {
 // @desc Get user profile
 // @route GET /api/users/profile
 // @access Private
-const getUserProfile = asyncHandler(async (req, res) => {
+export const getUserProfile = asyncHandler(async (req, res) => {
   const user = await User.findById(req.user._id);
 
   if (user) {
@@ -75,7 +75,7 @@ const getUserProfile = asyncHandler(async (req, res) => {
 // @desc Update user profile
 // @route PUT /api/users/profile
 // @access Private
-const updateUserProfile = asyncHandler(async (req, res) => {
+export const updateUserProfile = asyncHandler(async (req, res) => {
   const user = await User.findById(req.user._id);
 
   if (user) {
@@ -100,5 +100,3 @@ const updateUserProfile = asyncHandler(async (req, res) => {
     throw new Error('User not found');
   }
 });
-
-export { authUser, getUserProfile, registerUser, updateUserProfile };
