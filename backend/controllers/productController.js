d533bcdab35441755c318a9b1dc5ea24d2993d79
Daniel Segura <danielseguraf@gmail.com>
2022-08-22 18:44:36 -0500

    create and update product endpoints

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -42,9 +42,53 @@ export const deleteProductById = asyncHandler(async (req, res) => {
 });
 
 // @desc Create new product
-// @route DELETE /api/products
+// @route POST /api/products
 // @access Private/Admin
+export const createProduct = asyncHandler(async (req, res) => {
+  const product = new Product({
+    name: 'Sample name',
+    price: 0,
+    user: req.user._id,
+    image: '/images/sample.jpg',
+    brand: 'Sample brand',
+    category: 'Sample category',
+    countInStock: 0,
+    description: 'Sample description',
+    numReviews: 0,
+  });
 
-export const createNewProduct = asyncHandler(async (req, res) => {
-  const {} = req.body;
+  const createdProduct = await product.save();
+
+  if (createdProduct) {
+    res.status(201).json(createdProduct);
+  } else {
+    res.status(400);
+    throw new Error('Invalid product');
+  }
+});
+
+// @desc Update a product
+// @route PUT /api/products/:id
+// @access Private/Admin
+export const updateProduct = asyncHandler(async (req, res) => {
+  const { name, price, image, brand, category, description, countInStock } =
+    req.body;
+
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    product.name = name || product.name;
+    product.image = image || product.image;
+    product.brand = brand || product.brand;
+    product.category = category || product.category;
+    product.description = description || product.description;
+    product.price = price || product.price;
+    product.countInStock = countInStock || product.countInStock;
+
+    const updatedProduct = await product.save();
+    res.status(201).json(updatedProduct);
+  } else {
+    res.status(404);
+    throw new Error('Product not found');
+  }
 });
