11d0d9bc55b6fdf16e0595cda134c7eb64b1df71
Daniel Segura <danielseguraf@gmail.com>
2022-08-23 01:26:15 -0500

    product screen

---
+++ frontend/src/screens/ProductEditScreen.js
@@ -0,0 +1,114 @@
+import React, { useState, useEffect } from 'react';
+import { Link, useNavigate, useParams } from 'react-router-dom';
+import { Form, Button } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import Message from '../components/Message';
+import FormContainer from '../components/FormContainer';
+import Loader from '../components/Loader';
+import { getProductDetailStart } from '../features/productDetail/productDetailSlice';
+
+const ProductEditScreen = () => {
+  const [name, setName] = useState('');
+  const [brand, setBrand] = useState('');
+  const [category, setCategory] = useState('');
+  const [description, setDescription] = useState('');
+  const [price, setPrice] = useState();
+  const [countInStock, setCountInStock] = useState('');
+  const [isAdmin, setIsAdmin] = useState(false);
+
+  const dispatch = useDispatch();
+  const params = useParams();
+  const navigate = useNavigate();
+
+  const { loading, user, error } = useSelector((state) => state.userDetails);
+  const { token } = useSelector((state) => state.userLogin.userInfo);
+  const { success } = useSelector((state) => state.userUpdate);
+
+  useEffect(() => {
+    if (success) {
+      dispatch(userUpdateReset());
+      navigate('/admin/userlist');
+    } else {
+      if (!user.name || user._id !== params.id) {
+        dispatch(userDetailsStart({ id: params.id, token }));
+      } else {
+        setName(user.name);
+        setEmail(user.email);
+        setIsAdmin(user.isAdmin);
+      }
+    }
+  }, [
+    dispatch,
+    token,
+    params.id,
+    user.name,
+    user.email,
+    user.isAdmin,
+    user._id,
+    success,
+    navigate,
+  ]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const user = { name, email, isAdmin };
+    if (name && email) {
+      dispatch(userUpdateStart({ id: params.id, user, token }));
+    }
+  };
+
+  return (
+    <>
+      <Link to="/admin/userlist" className="btn btn-light my-3">
+        Go Back
+      </Link>
+      <FormContainer>
+        <h1>Edit Product</h1>
+        {loading ? (
+          <Loader />
+        ) : error ? (
+          <Message variant="danger">{error}</Message>
+        ) : (
+          <Form onSubmit={handleSubmit}>
+            <Form.Group controlId="name">
+              <Form.Label>Name</Form.Label>
+              <Form.Control
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+
+            <br />
+            <Form.Group controlId="email">
+              <Form.Label>Brand</Form.Label>
+              <Form.Control
+                type="email"
+                value={brand}
+                onChange={(e) => setBrand(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+
+            <br />
+            <Form.Group controlId="isAdmin">
+              <Form.Check
+                type="checkbox"
+                label="Is admin"
+                checked={isAdmin}
+                onChange={(e) => setIsAdmin(e.target.checked)}
+              ></Form.Check>
+            </Form.Group>
+
+            <br />
+            <Button type="submit" variant="primary">
+              Update
+            </Button>
+          </Form>
+        )}
+      </FormContainer>
+    </>
+  );
+};
+
+export default ProductEditScreen;
